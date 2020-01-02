1b88b665f6229123ec26e42d0797d37eee7a0470
yj <yjsoh@eng.ucsd.edu>
2020-01-01 19:51:01 -0800

    Simple script for running KC ycsb

---
+++ kyotocabinet/kcycsb.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+import subprocess, shlex
+import pandas as pd
+
+# execute a shell command, blocking, printing output
+def shellcmd(cmdstr, stdout=subprocess.PIPE):
+	cmds = shlex.split(cmdstr)
+	result = subprocess.run(cmds, stdout=stdout)
+	# print(result.stdout.decode("utf-8"))
+
+def load(workload):
+    ofname=workload+"_load.csv"
+    cmd = "./bin/ycsb load kyotocabinet -s -P {wkld}".format(wkld=workload)
+    print(cmd)
+    with open(ofname, 'wb') as f:
+        shellcmd(cmd, stdout=f)
+
+def run(workload):
+    ofname=workload+"_run.csv"
+    cmd = "./bin/ycsb run kyotocabinet -s -P {wkld}".format(wkld=workload)
+    print(cmd)
+    with open(ofname, 'wb') as f:
+        shellcmd(cmd, stdout=f)
+
+def verify(workload):
+    df = pd.read_csv(workload)
+    l = ["Return" in f for f in df.iloc[:,1]]
+    for ret in df.iloc[:,1][l]:
+        if(ret.strip() != "Return=OK"):
+            print(ret)
+            return False
+    return True
+
+if __name__ == "__main__":
+    workloads = range(ord('f'), ord('g'))
+    for wkld in workloads:
+        print(chr(wkld))
+        workload = "./workloads/workload" + chr(wkld)
+        load(workload)
+        assert(verify(workload + "_load.csv"))
+
+        run(workload)
+        assert(verify(workload + "_run.csv"))
+    print("No assertion error found. Done.")
