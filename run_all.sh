3b5efc6d6770b52e9e641d9b8e4ecd34f4f3fcc3
yj <yjsoh@eng.ucsd.edu>
2020-01-10 22:31:07 -0800

    Updated run_all.sh

--- run_all.sh
+++ run_all.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
 function sz_env {
-source /root/YJ/SubzeroApps/setenv.sh nova
+source ../SubzeroApps/setenv.sh nova
 }
 
 function build {
 
-bin=$1
-cd /root/YJ/kyotocabinet_$bin
+cd ../kyotocabinet_$1
 make clean && make -j`nproc` && sudo make install
 
-cd /root/YJ/kyotocabinet-java-1.24
+cd ../kyotocabinet-java-1.24
 make clean && make -j`nproc` && sudo make install
+
+cd ../YCSB
+mvn -pl site.ycsb:kyotocabinet-binding -am clean package
+
 }
 
 function run {
-for i in {20..27..2}; do
+for i in {20..21..2}; do
     echo $1_$((2 ** $i))
     ./kyotocabinet/kcycsb.py --fieldlength $((2 ** $i)) --fieldcount 1 --logdir $1_$((2 ** $i))
 done
 }
 
-# build patch
+#build patch
 run patch
